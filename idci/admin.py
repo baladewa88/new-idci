68c42ab440b29b14d4306793c54c0dd2430ab162
baladewa88 <sutadi.triputra@gmail.com>
2016-09-07 16:03:29 +0700

    Penambahan Parameter Admin

--- idci/admin.py
+++ idci/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 
 from .models import Affiliations, Urls, Keywords, Papers, Citations, Authors, Venues
 
-admin.site.register(Papers)
+class PapersAdmin(admin.ModelAdmin):
+    fieldsets = [('Description',{'fields':['title','abstract','year', 'venue', 'pages', 'volume', 'number','selfcites']}),
+                ('Affiliation',{'fields':['affiliasi','publisher','pubaddress']}),
+                 ]
+    list_filter = [('crawldate')]
+    list_display = ('id','title','affiliasi','ncites','selfcites')
+    ordering = ('-crawldate',)
+    
+admin.site.register(Papers, PapersAdmin)
 admin.site.register(Urls)
 admin.site.register(Keywords)
 admin.site.register(Affiliations)
